67fc0a9c8752536198cc8244083ea3b0759b38f2
Ramon Chiara <ramonchiara@gmail.com>
2018-03-21 22:32:39 -0300

    Filtrando os produtos.

--- public_html/js/loja.js
+++ public_html/js/loja.js
@@ -3,22 +3,42 @@ $(document).ready(function () {
     $('#preco-min').on('input', function () {
         var min = $('#preco-min').val();
         $('#preco-min-lbl').html(min);
+        mostrarProdutos();
     });
 
     $('#preco-max').on('input', function () {
         var max = $('#preco-max').val();
         $('#preco-max-lbl').html(max);
+        mostrarProdutos();
     });
+
+    var produtos = [];
 
     $.ajax({
         url: 'produtos.json',
         method: 'GET',
         success: function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var produto = data[i];
+            produtos = data;
+            mostrarProdutos();
+        }
+    });
+
+    function mostrarProdutos() {
+        var min = $('#preco-min').val();
+        var max = $('#preco-max').val();
+
+        $('#produtos').html('');
+
+        for (var i = 0; i < produtos.length; i++) {
+            var produto = produtos[i];
+            if (produto.preco >= min && produto.preco <= max) {
                 $('#produtos').append(`<li><a href="#${produto.id}">${produto.nome}</a> - R$ ${produto.preco}</li>`);
             }
         }
-    });
+
+        if ($('#produtos').html() === '') {
+            $('#produtos').append(`<li><p class="alert alert-warning">Nenhum produto encontrado.</p></li>`);
+        }
+    }
 
 });
