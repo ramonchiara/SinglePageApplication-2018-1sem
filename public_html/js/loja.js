dc62dd5bcd885fb0bf81e8d973c3da5be6343fd4
Ramon Chiara <ramonchiara@gmail.com>
2018-03-21 23:48:31 -0300

    Carregando os produtos a partir de um JSON.

--- public_html/js/loja.js
+++ public_html/js/loja.js
@@ -1,8 +1,14 @@
 var loja = angular.module('loja', []);
 
-loja.controller('principal', function ($scope) {
+loja.controller('principal', function ($scope, $http) {
 
     $scope.min = 0;
     $scope.max = 10000;
+
+    $scope.produtos = [];
+
+    $http.get('produtos.json').then(function (response) {
+        $scope.produtos = response.data;
+    });
 
 });
