d89e5698842b162821b834413014d0243ced86d5
Ramon Chiara <ramonchiara@gmail.com>
2018-04-26 10:52:08 -0300

    Os arquivos do projeto são "atualizáveis" (network) e as biblitecas são "fixas" (cache)

--- public_html/sw.js
+++ public_html/sw.js
@@ -13,17 +13,18 @@ toolbox.precache([
     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
     'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/1.0.3/angular-ui-router.min.js'
+    'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/1.0.3/angular-ui-router.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/sw-toolbox/3.6.1/sw-toolbox.js'
 ]);
+
+toolbox.router.get('index.html', toolbox.networkFirst);
+toolbox.router.get('js/loja.js', toolbox.networkFirst);
+toolbox.router.get('views/*', toolbox.networkFirst);
+toolbox.router.get('produtos.json', toolbox.networkFirst);
 
 toolbox.router.get('*', toolbox.cacheFirst);
 
 /*
-toolbox.router.get('index.html', toolbox.cacheFirst);
-toolbox.router.get('js/loja.js', toolbox.cacheFirst);
-toolbox.router.get('views/*', toolbox.cacheFirst);
-toolbox.router.get('produtos.json', toolbox.cacheFirst);
-
 toolbox.router.get('*', toolbox.networkFirst, {
     networkTimeoutSeconds: 5
 });
